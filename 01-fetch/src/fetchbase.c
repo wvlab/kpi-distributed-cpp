3d0cd43c4d706ca172654e1ec08d0f062203d79e
wvlab <me@wvlab.xyz>
2025-09-02 04:33:34 +0300

    lab 01 fetch: change unix target name to linux

--- 01-fetch/src/fetchbase.c
+++ 01-fetch/src/fetchbase.c
@@ -1,5 +1,9 @@
 #ifdef _WIN32
-    #include "src/fetchbase_win.c"
-#elif __unix__
-    #include "src/fetchbase_unix.c"
-#endif
+#include "src/fetchbase_win.c"
+#else
+#ifdef __linux__
+#include "src/fetchbase_linux.c"
+#else
+#error "incompatible target"
+#endif // #ifdef __linux__
+#endif // #ifdef _WIN32
